d5f7fde5cd1907c70bf1fd24c421dc0d56e291e9
Bishal <deepcsr4@gmail.com>
2021-12-05 16:23:25 +0530

    Added custom progressbar, added google sign in added animation

--- app/src/main/java/com/bishal/algobase/MainActivity.java
+++ app/src/main/java/com/bishal/algobase/MainActivity.java
@@ -1,16 +1,21 @@
 package com.bishal.algobase;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +25,21 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
         // remove the action bar and setting display to full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        progressBar=findViewById(R.id.progressBar);
+        //changing progressbar color
+        progressBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.black), PorterDuff.Mode.SRC_IN );
+        progressBar.setVisibility(View.VISIBLE);
+
 
 
         //splash delay function
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this, DashboardActivity.class);
+                Intent i = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(i);
                 finish();
+
 
             }
 
--- app/src/main/java/com/bishal/algobase/MenuActivity.java
+++ app/src/main/java/com/bishal/algobase/MenuActivity.java
@@ -1,16 +1,27 @@
 package com.bishal.algobase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class MenuActivity extends AppCompatActivity {
     ImageView slideDown_iv;
+    Button login_btn, logout_btn;
+    TextView hi_tv, name_tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +30,26 @@ public class MenuActivity extends AppCompatActivity {
 
         // remove the action bar and setting display to full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         final Animation slide_down = AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.slide_down);
 
         final Animation slide_up = AnimationUtils.loadAnimation(getApplicationContext(),
                 R.anim.slide_up);
 
-        slideDown_iv=findViewById(R.id.slideDown_iv);
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+
+
+        login_btn = findViewById(R.id.login_btn);
+        logout_btn = findViewById(R.id.logout_btn);
+        name_tv = findViewById(R.id.name_tv);
+        hi_tv = findViewById(R.id.hi_tv);
+
+
+        slideDown_iv = findViewById(R.id.slideDown_iv);
 
         slideDown_iv.setOnClickListener(new View.OnClickListener() {
 
@@ -38,6 +61,48 @@ public class MenuActivity extends AppCompatActivity {
 
             }
         });
+
+        if (googleSignInAccount != null) {
+            final String getUsername = googleSignInAccount.getDisplayName();
+            final String getGmail = googleSignInAccount.getEmail();
+
+
+            name_tv.setText(getUsername);
+
+
+            // if user is logged in we need to hide the log in button & display log out only since both button are overlapping
+            login_btn.setVisibility(View.GONE);
+            login_btn.setEnabled(false);
+
+
+            logout_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    googleSignInClient.signOut();
+                    startActivity(new Intent(MenuActivity.this, SignupActivity.class));
+                    finish();
+                }
+            });
+        }
+        else{
+            hi_tv.setText("");
+
+            // to re enable the buttons
+            logout_btn.setVisibility(View.GONE);
+            login_btn.setVisibility(View.VISIBLE);
+            login_btn.setEnabled(true);
+
+            login_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    googleSignInClient.signOut();
+                    startActivity(new Intent(MenuActivity.this, SignupActivity.class));
+                    finish();
+                }
+            });
+
+
+        }
 
 
     }

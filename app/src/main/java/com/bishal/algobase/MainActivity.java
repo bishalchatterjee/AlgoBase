bce85401836805c40cd7309696ed13fac79fd6be
Bishal <deepcsr4@gmail.com>
2021-12-05 17:01:13 +0530

    Minor Fixes & added shared preference to show signup activity on first time only

--- app/src/main/java/com/bishal/algobase/MainActivity.java
+++ app/src/main/java/com/bishal/algobase/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bishal.algobase;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,6 +17,7 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar progressBar;
+    SharedPreferences preferences;//to get preference and run the FeaturesActivity only for the first time
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
         // remove the action bar and setting display to full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
         progressBar=findViewById(R.id.progressBar);
         //changing progressbar color
         progressBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.black), PorterDuff.Mode.SRC_IN );
@@ -36,11 +41,24 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(i);
-                finish();
+                preferences = getSharedPreferences("PREFERENCE", MODE_PRIVATE);//name and mode(has to be MODE_PRIVATE)
+                Boolean isFirstTime = preferences.getBoolean("firstTime", true); //sets default true to firstTime variable if not present auto creates
 
+                if (isFirstTime) {//returns boolean type
+                    SharedPreferences.Editor editor = preferences.edit();//edit the preference as the first time is already checked
+                    editor.putBoolean("firstTime", false);
+                    editor.commit(); //commit changes to firstTime
 
+                    Intent i = new Intent(MainActivity.this, SignupActivity.class);
+                    startActivity(i);
+                    finish();
+
+                } else {
+                    Intent i = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(i);
+                    finish();
+
+                }
             }
 
         }, 1500);
